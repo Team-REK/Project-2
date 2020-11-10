12876f6c3fd8f567fb2e1f0c5a85ae00cb22f702
EricLiIIT <60123235+EricLiIIT@users.noreply.github.com>
2020-11-09 23:43:28 -0600

    udpate to js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 function init() {
-  var button = document.getElementById("submit_button");
+  var button = document.getElementById("button");
   button.addEventListener("click", function buttonPress() {
       var name = document.getElementById("name").value;
       var lastName = document.getElementById("lastName").value;
       var email = document.getElementById("email").value;
       if (name && lastName && email) {
-          alert("We will notify you when your iPhone ships");
+          alert("We'll see you soon!");
       } else {
           alert("Please fill out the form");
       }
   })
-  window.addEventListener('load', init);
 }
+window.addEventListener('load', init);
